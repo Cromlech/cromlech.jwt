ed85df2b4869c7c8f1d423994f92f6a7dd3ab8b8
Souheil CHELFOUH <trollfot@gmail.com>
2017-09-01 17:19:25 +0200

    Removed artifact code. Added tests

--- src/cromlech/jwt/components.py
+++ src/cromlech/jwt/components.py
@@ -5,14 +5,34 @@ import json
 from .utils import get_posix_timestamp, expiration_date
 from jwcrypto import jwk, jws, jwt
 from jwcrypto.common import json_encode, json_decode
+from jwcrypto.jwe import InvalidJWEData
+from jwcrypto.jwt import JWTExpired
 
 
+class TokenException(Exception):
+
+    def __init__(self, token):
+        self.token = token
+    
+
+class InvalidToken(TokenException):
+
+    def __str__(self):
+        return "Token %r could not be parse and/or interpreted." % self.token
+
+
+class ExpiredToken(TokenException):
+
+    def __str__(self):
+        return "Token %r is expired." % self.token
+
+    
 class JWTHandler(object):
 
     def __init__(self, auto_timeout=None):
         self.auto_timeout = auto_timeout
 
-    def create_payload(self, **ticket):
+    def create_payload(self, **data):
         tid = uuid.uuid4()
         payload = {
             'uid': str(tid),
@@ -22,11 +42,11 @@ class JWTHandler(object):
                 expiration_date(minutes=self.auto_timeout))
             payload['exp'] = int(exp)
 
-        ticket.update(payload)
-        return ticket
+        payload.update(data)
+        return payload
 
     @staticmethod
-    def generate_key(self, ktype='oct', size=256):
+    def generate_key(ktype='oct', size=256):
         return jwk.JWK.generate(kty=ktype, size=size)
 
     def create_signed_token(self, key, payload, alg="HS256"):
@@ -55,7 +75,10 @@ class JWTHandler(object):
         """Return the claims of a signed and encrypted token.
         """
         eclaims = self.verify(key, serial)
-        ST = jwt.JWT(key=key, jwt=eclaims)
+        try:
+            ST = jwt.JWT(key=key, jwt=eclaims)
+        except JWTExpired:
+            raise ExpiredToken(serial)
         return ST.claims
 
 
@@ -67,9 +90,8 @@ class JWTService(object):
         self.auto_deprecation = auto_deprecate and lifetime or None        
         self.handler = handler(auto_timeout=self.auto_deprecation)
 
-    def check_token(self, payload):
-        # Override for custom checks.
-	return True
+    def check_data(self, payload):
+        return True  # Override for custom checks
 
     def store(self, token):
         raise NotImplementedError(
@@ -79,31 +101,30 @@ class JWTService(object):
         raise NotImplementedError(
             'Please override this method in a subclass.')
     
-    def generate(self, payload):
+    def generate(self, data):
+        payload = self.handler.create_payload(**data)
         token = self.handler.create_encrypted_signed_token(self.key, payload)
+        return token.serialize()
 
     def refresh(self, *args):
         assert self.auto_deprecation is not None
         raise NotImplementedError(
             'Please override this method in a subclass.')
 
-    def authenticate(self, credentials):
+    def authenticate(self, token):
         """Returns a Principal object if credentials are valid
         """
-        if not isinstance(credentials, dict):
+        if token is None:
             return None
 
-        access_token = credentials.get('access_token')
-        if access_token is None:
-            return None
+        try:
+            payload = self.handler.decrypt_and_verify(self.key, token)
+        except InvalidJWEData:
+            raise InvalidToken(token)
 
-        payload = self.handler.decrypt_and_verify(self.key, access_token)
-        if not payload:
-            return None
-        else:
-            payload = json.loads(payload)
-
-        if self.check_token(payload) == True:
-            return AccessTokenHolder(payload)
+        if payload: 
+            data = json.loads(payload)
+            if self.check_data(data) == True:
+                return data
 
         return None
